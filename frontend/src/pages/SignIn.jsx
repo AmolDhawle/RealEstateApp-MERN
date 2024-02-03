17873340f8136c72a819d4e5adac7bce825276b0
Amol Dhawle <amoldhawle2@gmail.com>
2024-02-04 00:03:28 +0530

    created and added OAuth component

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart,  signInSuccess, signInFailure, } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 const SignIn = () => {
 
@@ -49,9 +50,14 @@ const SignIn = () => {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign In'}</button>
+          <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+          <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+          <button disabled={loading} 
+                  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          >
+            {loading ? 'Loading...' : 'Sign In'}
+          </button>
+          <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Don&apos;t have an account?</p>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 const SignUp = () => {
 
@@ -50,10 +51,15 @@ const SignUp = () => {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
-        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign Up'}</button>
+          <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+          <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+          <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+          <button disabled={loading} 
+                  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          >
+            {loading ? 'Loading...' : 'Sign Up'}
+          </button>
+          <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Already have an account?</p>

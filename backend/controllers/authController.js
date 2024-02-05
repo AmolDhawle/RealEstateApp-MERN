b2cf139f5ab1c84314a49c16bbfff3ac72b8a7f3
Amol Dhawle <amoldhawle2@gmail.com>
2024-02-05 16:14:23 +0530

    increased timeout duration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,11 +3,15 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
 
+setTimeout(() => {
+  
+}, 30000);
+
 export const signup = async (req, res, next) => {
    
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword});
+    const newUser = new User({ username, email, password: hashedPassword}).timeout(30000); 
     try {
         await newUser.save();
         res.status(201).json({message: 'User created successfully'});
@@ -20,7 +24,7 @@ export const signin = async (req, res, next) => {
     const  { email, password } = req.body;
 
     try {
-        const validUser = await User.findOne({ email })
+        const validUser = await User.findOne({ email }).timeout(30000); 
         if (!validUser) return next(errorHandler(404, 'User not found!'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword)  return next(errorHandler(401, 'Wrong credentials!'));
@@ -38,7 +42,7 @@ export const signin = async (req, res, next) => {
 
 export const google = async ( req, res, next ) => {
     try {
-      const user = await User.findOne({email: req.body.email});
+      const user = await User.findOne({email: req.body.email}).timeout(30000);
       if(user) {
         const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
         const { password: pass, ...rest} = user._doc;

b95c39786232be7c437866329ea8e5faaed35cd2
Amol Dhawle <amoldhawle2@gmail.com>
2024-02-05 21:08:59 +0530

    changed color on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+          Find your next <span className='bg-[#0f5a80]'>perfect</span>
           <br />
           place with ease
         </h1>

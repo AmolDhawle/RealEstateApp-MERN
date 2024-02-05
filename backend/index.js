b7a057eeec6c7d1fbfd8032155a6cee47e465555
Amol Dhawle <amoldhawle2@gmail.com>
2024-02-05 15:01:43 +0530

    added reinstallation

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/userRoutes.js';
 import authRouter from './routes/authRoutes.js';
 import listingRouter from './routes/listingRoutes.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 }).catch((err) => {
     console.log(err);
 });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -27,6 +30,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500;
